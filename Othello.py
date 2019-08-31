0c45470d78289cd41fcf9720a6fe5e2b3f48640c
cspradli <cspradli@unca.edu>
2019-08-31 15:49:54 -0400

    added pieces support

--- Othello.py
+++ Othello.py
@@ -1,4 +1,10 @@
 class Board():
 
     directions = [(1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1, 0), (-1,-1), (0,1)]
-    
+
+    def __init__(self):
+        self.pieces = [None]*8
+        for i in range(8):
+            self.pieces[i] = [0]*8
+        
+        
