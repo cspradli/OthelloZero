8676f04960f9d4e41854d9e693111b122fcc32fa
cspradli <cspradli@unca.edu>
2019-09-24 10:05:40 -0400

    implementeed pseudocode

--- Othello.py
+++ Othello.py
@@ -24,13 +24,71 @@ class Board():
         #Returns a list of moves#
         color = self.color
 
-        
+    def makeFlips(self, color, move, origin):
+        flipList = [origin]
+        for x,y in self.incrementMove(origin, direction):
+            if self.pieces[x][y] == color and len(flipList) < 1:
+                return []
+            elif self.pieces[x][y] == -(color):
+                flipList.append((x,y))
+            elif self.pieces[x][y] == color and len(flipList) > 1:
+                return flipList
     
-    def findMoves(self, originPiece, direction):
-        #Finds moves based on origin tuple given and the direction wanted to go#
-        (x, y) = originPiece
-        
 
+    def executeMove(self color, move):
+        flipMoves = []
+        for directions in self.directions:
+            flipList = self.makeFlips(color, move, direction)
+            if flipList > 0:
+                flipMoves = flipList
+        for x,y in flipMoves:
+            self.pieces[x][y] = color
+
+    def getValidMoves(self, color):
+        validMoves = set()
+        for x in range(8):
+            for y in range(8):
+                if self.pieces[x][y] == color:
+                    newValidMoves = findAllMoves((x,y))
+                validMoves.update(newValidMoves)
+        return list(validMoves)
+
+    def findAllMoves(self, color, (x,y)):
+        possibleMoves = []
+        for direction in directions:
+            oneMove = findMove((x,y), direction)
+            #if oneMove is validMove
+                possibleMoves.append(oneMove)
+        return possibleMoves
+
+    
+    def findMove(self, originPiece, direction):
+        #Finds moves based on origin tuple given and the direction wanted to go#
+        x, y = originPiece
+        flipMoves = []
+        for x,y in incrementMove(origin, direction):
+            if self.pieces[x][y] == color and flipMoves:
+                return x,y
+            elif self.pieces[x][y] == -(color):
+                flipMoves.append((x,y))
+            elif self.pieces[x][y] == color:
+                return None
+        
+    def incrementMove(move, direction):
+        moves = list()
+        x, y = move
+        direcX, direcY = direction
+        print(x)
+        print(y)
+        print(direcX)
+        print(direcY)
+        x+=direcX
+        y+=direcY
+        while x>=0 and x<8 and y>=0 and y<8:
+            moves.append((x,y))
+            x+=direcX
+            y+=direcY
+        print(moves)
 
     def countNum(self):
         # Counts the number of stones each player has
