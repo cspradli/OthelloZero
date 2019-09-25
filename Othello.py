16303667786a00390239f0b2daf610f1f4af72a5
cspradli <cspradli@unca.edu>
2019-09-25 15:11:19 -0400

    finally got something working

--- Othello.py
+++ Othello.py
@@ -1,13 +1,11 @@
 class Board():
     # All 8 directions for the agent to look/go
-    directions = [(1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1, 0), (-1,-1), (0,1)]
+    directions = [(1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1, 0), (-1,1), (0,1)]
 
-    def __init__(self, color):
+    def __init__(self):
 
-        self.color = color
         #initializes 2-D array showing the pieces
         self.pieces = [None]*8
-
         for i in range(8):
             self.pieces[i] = [0]*8
         
@@ -16,36 +14,34 @@ class Board():
         self.pieces[4][3] = -1
         self.pieces[3][3] = 1
         self.pieces[4][4] = 1
-        print(self.pieces)
 
     def __getitem__(self, index):
-        return self.pieces[index]
+        return self.pieces[index
+        
+        ]
+    def getSquares(self, color):
+        squares = []
+        for y in range(8):
+            for x in range(8):
+                if self.pieces[x][y] == color:
+                    squares.append((x,y))
+                    print(x, y)
+        print(squares)
     
-    def generateMoves(self):
-        #Looks for valid moves at current board state#
-        #Returns a list of moves#
-        color = self.color
-
     def makeFlips(self, color, direction, origin):
-
+        #Gets a list of flips given color, direction, and origin
         flipList = [origin]
 
         for x,y in self.incrementMove(origin, direction):
             print("Working on ")
             print(self.pieces[x][y])
-            if self.pieces[x][y] == 0 :
-                print("None found")
-                print(flipList)
-                return flipList
-
             if self.pieces[x][y] == -color:
-                print("Appended")
                 flipList.append((x,y))
-
+            elif (self.pieces[x][y] == 0 or (self[x][y] == color and len(flipList) == 1)):
+                break
             elif self.pieces[x][y] == color and len(flipList) > 1:
-                print(flipList)
-                return flipList
-    
+                return flips
+        return []
 
     def executeMove(self, color, move):
         flipMoves = []
@@ -57,54 +53,45 @@ class Board():
             self.pieces[x][y] = color
 
     def getValidMoves(self, color):
-
+        #Finds all valid moves given a color#
         validMoves = set()
-
-        for x in range(8):
-            for y in range(8):
-                if self.pieces[x][y]==color:
-                    newValidMoves = self.findAllMoves(color, (x,y))
-                    validMoves.update(newValidMoves)
-
+        for square in self.getSquares(color):
+            newmoves = self.findAllMoves(color, origin)
+            validMoves.update(newmoves)
         return list(validMoves)
 
     def findAllMoves(self, color, origin):
-
         possibleMoves = []
-        x, y = origin
-
         if color==0:
             return None
-        
         for direction in self.directions:
-            oneMove = self.findMoves(origin, direction)
+            oneMove = self.findMoves(color, origin, direction)
             if oneMove:
                 possibleMoves.append(oneMove)
-
         return possibleMoves
 
     
-    def findMoves(self, originPiece, direction):
-        #Finds moves based on origin tuple given and the direction wanted to go#
-        x, y = originPiece
+    def findMoves(self, color, originPiece, direction):
+        #Finds the endpoint of a legal move given move and direction#
+        x,y = originPiece
         flipMoves = []
 
-        for x,y in self.incrementMove(originPiece, direction):
-            if self.pieces[x][y] == color and flipMoves:
-                return (x,y)
-            elif self.pieces[x][y] == -(color):
-                flipMoves.append((x,y))
-            elif self.pieces[x][y] == color:
+        for x,y in Board.incrementMove(originPiece, direction):
+            if self.pieces[x][y] == 0 and flipMoves:
+                return (x, y)
+            elif (self.pieces[x][y] == color or (self.pieces[x][y] == 0 and not flipMoves)):
                 return None
-        
-    def incrementMove(self, move, direction):
+            elif self[x][y].pieces == -color:
+                flipMoves.append((x,y))
+    
+    @staticmethod
+    def incrementMove(move, direction):
         moves = list()
         x, y = move
         direcX, direcY = direction
-        x+=direcX
-        y+=direcY
+        x += direcX
+        y += direcY
         while x>=0 and x<8 and y>=0 and y<8:
-            print(x, y)
             moves.append((x,y))
             x+=direcX
             y+=direcY
@@ -136,7 +123,7 @@ class Board():
             print(str(y+1) + ' |', end = ''),
             for x in range(8):
                 # Get the piece to print
-                piece = self.pieces[x][y]
+                piece = self[x][y]
                 if piece == -1: 
                     print(" B ", end = ''),
                 elif piece == 1: 
@@ -148,10 +135,11 @@ class Board():
         print("    A  B  C  D  E  F  G  H")
 
 if __name__ == '__main__':
-    board = Board(1)
+    board = Board()
     board.display()
     board.countNum()
-    #board.incrementMove((3,3), (1,1))
-    board.makeFlips(1, (0,1), (3,3))
-    board.executeMove(1, (3,5))
-    board.display()
+    board.incrementMove((3,3), (1,1))
+    #board.getValidMoves(-1)
+    #board.makeFlips(1, (0,1), (3,3))
+    #board.executeMove(1, (3,5))
+    board.getSquares(1)
