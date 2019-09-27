586fd938eed5745faac60d9245f0990bec377d33
cspradli <cspradli@unca.edu>
2019-09-27 14:28:13 -0400

    added comment to main

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+"""
+    OthelloZero: main
+    Author: Caleb Spradlin
+    Date: 27/09/19
+    This file contains the main driver for the OthelloZero system, intiializes a board, and a game to run the board
+"""
+
 from OthelloGame import OthelloGame as Game
 from OthelloLogic import Board
 from OthelloIO import get_char_col, split_string
