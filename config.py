104c9450ff9a7f308194e2ba8c32e1da75b357ad
cspradli <cspradli@unca.edu>
2019-10-08 21:33:14 -0400

    wokred on nnet more

---
+++ config.py
@@ -0,0 +1,22 @@
+class config(object):
+
+    num_iterations = 4
+    num_games = 30
+    num_mcts_sims = 30
+    c_puct = 1
+    val = 0.0001
+    momentum = 0.9
+    learn_rate = 0.01
+    polict_val = 0.0001
+    temp_init = 1
+    temp_final = 0.001
+    temp_thresh = 10
+    epochs = 10
+    batch_size = 128
+    dirilecht_alpha = 0.3
+    epsilon = 0.25
+    models_direc = "./models/"
+    num_eval_games: 12
+    eval_win_rate = 0.55
+    load_model = 1
+    
--- nnet.py
+++ nnet.py
@@ -22,4 +22,56 @@ class othellonnet():
             self.inputBoard = tf.placeholder(tf.float32, shape=[None, self.board_X, self.board_Y])
             self.dropout = tf.placeholder(tf.float32)
             self.isTraining = tf.placeholder(tf.bool, name="is_training")
+
+            #Layer to be taken as input by the model
+            input_layer = tf.reshape(self.inputBoard, -1, [self.board_X, self.board_Y, 1])
+
+            #Initial convolutional bloack
+            convolution1 = tf.layers.conv2d(
+                inputs = input_layer,
+                filters = 256,
+                kernel_size = [3, 3],
+                padding= "same",
+                strides= 1
+            )
+
+            batch_norm1 = tf.layers.batch_normalization(
+                input = conv1,
+                training=self.isTraining
+            )
+
+            relu1 = tf.nn.relu(batch_norm1)
+            in_out = relu1
+
+            for i in range(config.resnet_blocks):
+                #residual block towers
+                conv2 = tf.layers.conv2d(
+                    inputs = in_out
+                    filters = 256
+                    kernel_size = [3,3]
+                    padding="same"
+                    strides=1
+                )
+
+                batch_norm2 = tf.layers.batch_normalization(
+                    inputs = conv2
+                    training=self.isTraining
+                )
+                relu2 = tf.nn.relu(batch_norm2)
+                conv3 = tf.layers.conv2d(
+                    inputs = relu2,
+                    filters = 256,
+                    kernel_size = [3,3],
+                    padding="same",
+                    strides=1
+                )
+                batch_norm3 = tf.layers.batch_normalization(
+                    inputs = conv3,
+                    trainable=self.isTraining
+                )
+                resnet_skip = tf.add(batch_norm3, in_out)
+                resnet_in_out = tf.nn.relu(resnet_skip)
+
+        def conv2d(self, x, out_channels, padding):
+            return tf.layers.conv2d(x, out_channels, kernel_size=[3,3], padding=self.padding, use_bias=False)
             
