b01241acbd71edd68b74fa8d0800ab878b83e91c
cspradli <cspradli@unca.edu>
2019-10-11 14:47:53 -0400

    added res net

--- othellonnet2.py
+++ othellonnet2.py
@@ -48,3 +48,50 @@ class othellonnet():
     def calc_loss(self):
         self.target_pis = tf.placeholder(tf.float32, shape=[None, self.action_size])
         self.target_vs = tf.placeholder(tf.float32, shape=[None])
+        self.loss_pi = tf.losses.softmax_cross_entropy(self.target_pis, self.pi)
+        self.loss_vi = tf.losses.mean_squared_error(self.target_vs, tf.reshape(self.v, shape[-1,]))
+        self.total_loss = self.loss_pi + self.loss_vi
+        update = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update):
+            self.train_step = tf.train.AdamOptimizer(self.config.lr).minimize(self.total_loss)
+
+def residual_tower():
+    def __init__(self, game):
+        self.board_X, self.board_Y = game.get_board_size()
+        self.action_size = game.get_action_size()
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            self.board = tf.placeholder(tf.float32, shape=[None, self.board_X, self.board_Y])
+            self.drop = tf.placeholder(tf.float32)
+            self.isTraining = tf.placeholder(tf.bool, name="is_training")
+
+            x_im = tf.reshape(self.input_board, [-1, self.board_X, self.board_Y, 1])
+            x_im = tf.layers.conv2d(x_im, config.num_channels, kernel_size = (3,3), strides=(1,1), name='conv', padding='same', use_bias=False)
+            x_im = tf.layers.batch_normalization(x_im, axis=1, name='conv_bn', training=self.isTraining)
+            x_im = tf.nn.relu(x_im)
+
+            res_tow = self.residual_block(inputLayer = x_im, kernel_size = 3, filters=config.num_channels, stage=1, block='a')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='b')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='c')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='d')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='e')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='f')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='g')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='h')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='i')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='j')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='k')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='l')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='m')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='n')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='o')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='p')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='q')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='r')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='s')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='t')
+            res_tow = self.residual_block(inputLayer = res_tow, kernel_size = 3, filters=config.num_channels, stage=1, block='u')
+
+            pol =tf.layers.conv2d(res_tow, 2, kernel_size = (1,1), strides=(1,1), name='pi', padding='same', use_bias=False)
+            pol = tf.layers.batch_normalization(pol, axis=3, name='bn', training=self.isTraining)
+            
